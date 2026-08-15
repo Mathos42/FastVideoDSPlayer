d3e8504fc00d7381f3e85e6e9cf889984364ac07
Mathos42 <rvlogiciels@gmail.com>
2026-08-15 23:58:55 +0200

    Add files via upload

---
+++ arm9/source/main.cpp
@@ -0,0 +1,148 @@
+#include <nds.h>
+#include <nds/fifocommon.h>
+#include <stdio.h>
+#include <string.h>
+#include "../../common/ipc.h"
+#include "FastVideo/fvDecoder.h"
+#include "FastVideo/fvMcData.h"
+#include "FastVideo/fvPlayer.h"
+#include "mpu.h"
+#include "gui/PlayerController.h"
+#include "../../common/twlwram.h"
+
+static DTCM_BSS fv_player_t sPlayer;
+
+static PlayerController* sPlayerController;
+
+extern u8 gDldiStub[];
+
+// scratch buffer used to receive the previous/next path found by the arm7
+// (must be writable by the arm7 CPU, so plain main RAM, and cacheline
+// aligned so we can safely invalidate it)
+static char sAdjacentPath[FV_MAX_PATH_LEN] ALIGN(32);
+
+static bool sCanUseWram;
+
+// Loads and starts the video at path. Destroys/replaces the current player
+// and controller as needed. Returns false if the video could not be loaded
+// (in which case there is no active player/controller anymore).
+static bool loadAndStartVideo(const char* path)
+{
+    if (sPlayerController)
+    {
+        delete sPlayerController;
+        sPlayerController = NULL;
+        fv_destroyPlayer(&sPlayer);
+    }
+
+    if (!fv_initPlayer(&sPlayer, path, sCanUseWram))
+        return false;
+
+    sPlayerController = new PlayerController(&sPlayer);
+    sPlayerController->Initialize();
+    fv_startPlayer(&sPlayer);
+    return true;
+}
+
+// Asks the arm7 for the previous/next ".fv" file (alphabetically) in the
+// same folder as the video currently playing, and switches to it if found.
+// If no other ".fv" file exists, the current video keeps playing.
+static void switchToAdjacentVideo(bool next)
+{
+    fifoSendValue32(FIFO_USER_01, IPC_CMD_PACK(next ? IPC_CMD_FIND_NEXT_FILE : IPC_CMD_FIND_PREV_FILE,
+                                               (u32)sAdjacentPath));
+    fifoWaitValue32(FIFO_USER_01);
+    u32 found = fifoGetValue32(FIFO_USER_01) & IPC_CMD_ARG_MASK;
+    if (!found)
+        return; // no other video found next to the current one, keep playing
+
+    DC_InvalidateRange(sAdjacentPath, sizeof(sAdjacentPath));
+    loadAndStartVideo(sAdjacentPath);
+}
+
+int main(int argc, char** argv)
+{
+    DC_FlushAll();
+
+    mpu_enableVramCache();
+
+    bool canUseWram = false;
+    if (isDSiMode() && twr_isUnlocked())
+    {
+        twr_setBlockMapping(TWR_WRAM_BLOCK_A, 0x03000000, 0x40000, TWR_WRAM_BLOCK_IMAGE_SIZE_256K);
+        twr_setBlockMapping(TWR_WRAM_BLOCK_B, 0x03100000, 0x40000, TWR_WRAM_BLOCK_IMAGE_SIZE_256K);
+        twr_setBlockMapping(TWR_WRAM_BLOCK_C, 0x03140000, 0x40000, TWR_WRAM_BLOCK_IMAGE_SIZE_256K);
+        mpu_enableTwlWramCache();
+        canUseWram = true;
+    }
+
+    fifoSetValue32Handler(FIFO_USER_01, NULL, NULL);
+
+    // handshake
+    fifoSendValue32(FIFO_USER_01, IPC_CMD_PACK(IPC_CMD_HANDSHAKE, 0));
+    fifoWaitValue32(FIFO_USER_01);
+    u32 handShake = fifoGetValue32(FIFO_USER_01);
+
+    if (canUseWram && (handShake & IPC_CMD_ARG_MASK) == 0)
+        canUseWram = false;
+
+    sCanUseWram = canUseWram;
+
+    if (!isDSiMode())
+    {
+        // setup dldi on arm7 if not on dsi
+        DC_FlushRange(gDldiStub, 16 * 1024);
+        fifoSendValue32(FIFO_USER_01, IPC_CMD_PACK(IPC_CMD_SETUP_DLDI, (u32)gDldiStub));
+        fifoWaitValue32(FIFO_USER_01);
+        fifoGetValue32(FIFO_USER_01);
+    }
+
+    videoSetModeSub(MODE_0_2D);
+    vramSetBankH(VRAM_H_SUB_BG);
+    vramSetBankI(VRAM_I_SUB_SPRITE);
+
+    consoleInit(NULL, 2, BgType_Text4bpp, BgSize_T_256x256, /*0, 1*/ 2, 1, false, true);
+
+    // iprintf("FastVideoDS Player by Gericom\n\n");
+
+    vramSetBankA(VRAM_A_LCD);
+    vramSetBankB(VRAM_B_LCD);
+    vramSetBankC(VRAM_C_LCD);
+    vramSetBankD(VRAM_D_LCD);
+    vramSetBankE(VRAM_E_LCD);
+
+    for (int i = 0; i < 3 * 128 * 1024; i += 4)
+        *(vu32*)((u32)VRAM_A + i) = 0x80008000;
+
+    const char* filePath;
+
+    if (isDSiMode())
+        filePath = "sd:/testVideo.fv";
+    else
+        filePath = "fat:/testVideo.fv";
+
+    if (argc >= 2)
+        filePath = argv[1];
+
+    // iprintf("Playing %s\n", filePath);
+    if (loadAndStartVideo(filePath))
+    {
+        while (sPlayerController)
+        {
+            PlayerController::NavAction action = sPlayerController->Update();
+            if (action == PlayerController::NAV_ACTION_NEXT)
+                switchToAdjacentVideo(true);
+            else if (action == PlayerController::NAV_ACTION_PREV)
+                switchToAdjacentVideo(false);
+        }
+    }
+    if (sPlayerController)
+    {
+        delete sPlayerController;
+        fv_destroyPlayer(&sPlayer);
+    }
+    while (1)
+        swiWaitForVBlank();
+
+    return 0;
+}
